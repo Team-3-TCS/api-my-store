ff0581b90ec3563a74b97048e0803bbc8f43ba69
andrespa20 <58963032+andrespa20@users.noreply.github.com>
2021-04-08 15:37:16 -0500

    Add files via upload

---
+++ controllers/controlador_categoria.py
@@ -0,0 +1,7 @@
+from conexion import *
+import sys
+sys.path.append("..")
+from model.categoria import *
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ controllers/controlador_vendedor.py
@@ -0,0 +1,7 @@
+from conexion import *
+import sys
+sys.path.append("..")
+from model.vendedor import *
+
+if __name__=="__main__":
+    app.run(debug=True)
