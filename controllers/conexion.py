d3181db758b1c172cddfbd4aa759eaccc9b0f346
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-04-10 19:33:04 -0500

    agregando cadena de conexion en la nube

--- controllers/conexion.py
+++ controllers/conexion.py
@@ -1,9 +1,9 @@
 from flask import Flask, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+# jdbc:mysql://68.183.23.64:3306/?user=mystorebd
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root@localhost/dbmystore'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://mystorebd:Server123_@68.183.23.64:3306/dbMyStore'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 ma=Marshmallow(app)
