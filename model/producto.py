468796315bea3da34a43856885bfc2dc626ad462
jair najarro <jair.najarro@unmsm.edu.pe>
2021-04-17 00:55:22 -0500

    correcciones realizadas en producto.py

--- model/producto.py
+++ model/producto.py
@@ -31,7 +31,7 @@ class Producto(db.Model):
 
 class Producto_Schema(ma.Schema):
     class Meta:
-        fields=('id_producto','id_categoria','id_vendedor,nombre','descripcion','precio','imagen','stock','estado_activacion','fecha_adicion','fecha_modificacion')
+        fields=('id_producto','id_categoria','id_vendedor','nombre','descripcion','precio','imagen','stock','estado_activacion','fecha_adicion','fecha_modificacion')
 
 producto_schema=Producto_Schema()
 productos_schema=Producto_Schema(many=True)
@@ -78,7 +78,7 @@ def get_producto_nombre(nombre):
 @app.route('/producto/<nombre>',methods=['PUT'])
 def update_producto(id_producto):
     producto=Compra.query.get(nombre)
-    id_categoria=request.json['nombre']
+    id_categoria=request.json['id_categoria']
     id_vendedor=request.json['id_vendedor']
     nombre=request.json['nombre']
     descripcion=request.json['descripcion']
