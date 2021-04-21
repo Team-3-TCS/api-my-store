49c0d6b60b9710be6f38d2d22fc8cd8b10d3b5a9
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-04-21 18:46:08 -0500

    agregando los cors

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 from .config import config_by_name
 
@@ -13,6 +14,7 @@ ma=Marshmallow()
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
